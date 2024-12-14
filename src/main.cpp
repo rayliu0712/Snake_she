5dc4106ba541702400642308ea3dd24d458311bf
Ray Liu <rayliu0712@gmail.com>
2024-12-14 17:00:20 +0800

    remove "using namespace std" in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include "game.hpp"
 #include <memory>
-using namespace std;
 
 // flags: "--fps" "--reward-frequency"
 int main(int argc, char **argv) {
-    static auto game = make_unique<Game>(argc, argv);
+    static auto game = std::make_unique<Game>(argc, argv);
     game->welcome();
     game->adjustMapSize();
     game->playLoop();
